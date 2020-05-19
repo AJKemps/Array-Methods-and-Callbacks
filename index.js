d686d10e8a02fc68030215170928bf58f9bea1b5
Alex Kemper <Kemper@Alexanders-MacBook-Air.local>
2020-05-19 16:00:47 -0400

    task 3 complete

--- index.js
+++ index.js
@@ -52,13 +52,15 @@ console.log(getFinals);
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
-
-    /* code here */
-
+function getYears(arr) {
+    let years = [];
+    for ( let i = 0; i < arr.length; i++) {
+        years.push(arr[i].Year);
+    };
+    return years;
 };
 
-getYears();
+console.log(getYears(getFinals));
 
 /* Task 5: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
